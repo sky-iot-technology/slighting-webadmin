381877e442812575e02e7eca6b3e49cf8bec2d28
Son Tran <son.tran@groovetechnology.com>
2025-11-12 11:54:44 +0700

    Refactor role page to remove calendar table and replace with a simple header. This change simplifies the UI for the role section of the dashboard.

--- src/app/dashboard/role/page.tsx
+++ src/app/dashboard/role/page.tsx
@@ -1,13 +1,7 @@
-import { CalendarTable } from '@/features/calendar/components/calendar-tables';
-import { columns } from '@/features/calendar/components/calendar-tables/device-calendar-columns';
-import { calendars } from '@/features/calendar/components/fake';
-
 export default function page() {
   return (
-    <CalendarTable
-      data={calendars}
-      totalItems={calendars.length}
-      columns={columns}
-    />
+    <div>
+      <h1>Role</h1>
+    </div>
   );
 }
